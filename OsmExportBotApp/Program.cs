e0c4a9129c55223f77467230acb4485395253e1a
Almaz <zetx16@gmail.com>
2023-11-04 13:23:37 +0300

    Migrate to .net 7

--- OsmExportBotApp/Program.cs
+++ OsmExportBotApp/Program.cs
@@ -1,24 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading;
+﻿
+Timer timer;
+long interval = 1000;
 
-namespace OsmExportBotApp
-{
-    class Program
-    {
-        static Timer timer;
-        static long interval = 1000;
-
-        static void Main(string[] args)
-        {
-            OsmExportBot.Config.Token = "TOKEN";
-            OsmExportBot.Config.RulesFolder = "Queries\\";
-            timer = new Timer(new TimerCallback(OsmExportBot.Bot.Run), null, 0, interval);
-            //OsmExportBot.Bot.Run( null );
-            Console.ReadLine();
-        }
-    }
-}
+OsmExportBot.Config.Token = "TOKEN";
+OsmExportBot.Config.RulesFolder = "Queries\\";
+timer = new Timer(new TimerCallback(OsmExportBot.Bot.Run), null, 0, interval);
+//OsmExportBot.Bot.Run( null );
+Console.ReadLine();
