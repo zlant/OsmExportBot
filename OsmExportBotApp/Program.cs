aad45a534557b45ab7c083cf421a2c6174eb17c0
Almaz <zetx16@gmail.com>
2023-11-05 11:30:07 +0300

    Add docker

--- OsmExportBotApp/Program.cs
+++ OsmExportBotApp/Program.cs
@@ -1,8 +1,7 @@
-﻿
-Timer timer;
-long interval = 1000;
+﻿OsmExportBot.Config.Token = "TOKEN";
 
-OsmExportBot.Config.Token = "TOKEN";
-timer = new Timer(new TimerCallback(OsmExportBot.Bot.Run), null, 0, interval);
-//OsmExportBot.Bot.Run( null );
-Console.ReadLine();
+while (true)
+{
+    OsmExportBot.Bot.Run(null);
+    await Task.Delay(TimeSpan.FromSeconds(1));
+}
